86f5c0622c76dc16f95260ccafb68885942d247a
Madeleine Nilsson <59937917+m3pan@users.noreply.github.com>
2021-12-02 12:46:38 +0100

    second star!

--- main.cpp
+++ main.cpp
@@ -7,11 +7,21 @@ std::vector<int> readFile(std::string file);
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::vector<int> measurements = readFile("../day1_input.txt");
+    // Part One
     int increase{0};
-    for(int i = 0; i<(measurements.size()); i++){
-        if (measurements[i] < measurements[i+1]) {++increase;}
+    for (int i = 0; i < (measurements.size()); i++) {
+        if (measurements[i] < measurements[i + 1]) { ++increase; }
     }
     std::cout << "How many measurements are larger than the previous measurement? " << increase << std::endl;
+
+    // Part two
+    increase = 0;
+    for (int i = 0; i < (measurements.size()-2); i++) {
+        int A = measurements[i] + measurements[i+1] + measurements[i+2];
+        int B = measurements[i+1] + measurements[i+2] + measurements[i+3];
+        if (A < B) {++increase;}
+    }
+    std::cout << "How many sums are larger than the previous sum? " << increase << std::endl;
 }
 
 std::vector<int> readFile(std::string file) {
